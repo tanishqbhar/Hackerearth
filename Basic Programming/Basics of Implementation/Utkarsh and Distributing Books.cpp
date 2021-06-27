0ddf0fc3ad89c3bc56ea48d3a7a5f73cf5178778
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-06-27 22:20:51 +0530

    Create Utkarsh and Distributing Books.cpp

---
+++ Basic Programming/Basics of Implementation/Utkarsh and Distributing Books.cpp
@@ -0,0 +1,26 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		int n, mn = INT_MAX, sum = 0;
+		cin >> n;
+		vector<int>arr(n);
+		for(int i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+			sum += arr[i];
+			mn = min(mn, arr[i]);
+		}
+		cout << mn - 1 << " " << sum - n << endl;
+	}
+}
