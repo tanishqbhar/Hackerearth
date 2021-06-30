c33c6abf2b09f8edc03bdd34e08266a376d29c7c
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-06-30 23:31:17 +0530

    Create Palindromic Numbers.cpp

---
+++ Basic Programming/Basics of Implementation/Palindromic Numbers.cpp
@@ -0,0 +1,32 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+	{
+        int a, b, n = 0;
+		cin >> a >> b;
+        for(int i = a; i <= b; i++)
+		{
+            int c = i, sum = 0;
+            while(c > 0)
+			{
+                int x = c % 10;
+                sum = 10 * sum + x;
+                c /= 10;
+            }
+            if(i == sum)
+                n++;
+        }
+        cout << n << endl;
+    }
+    return 0;
+}
