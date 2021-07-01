13831f705661a5c1695e38c2b3fe4c375332cefc
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-07-01 18:10:55 +0530

    Create Special Sum.cpp

---
+++ Basic Programming/Basics of Implementation/Special Sum.cpp
@@ -0,0 +1,20 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int T ;
+    cin >> T;
+    while(T--)
+    {
+        long long N ;
+        cin >> N;
+        cout << N << endl;
+    }
+    return 0;
+}
