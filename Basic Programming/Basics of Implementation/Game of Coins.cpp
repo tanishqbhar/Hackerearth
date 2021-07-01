ed4c05416344c1027f0eb6f0003ecc763723d744
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-07-01 08:38:00 +0530

    Create Game of Coins.cpp

---
+++ Basic Programming/Basics of Implementation/Game of Coins.cpp
@@ -0,0 +1,18 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t, n;
+	cin >> t;
+	while(t--)
+	{
+		cin >> n;
+		cout << "Alice" << endl;
+	}
+}
